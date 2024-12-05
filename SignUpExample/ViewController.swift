7516f60a23619862624a0e5abb61190c1e7a6d14
Hun <ksh26271@naver.com>
2024-12-05 17:48:17 +0900

    [config] 프로젝트 세팅

---
+++ SignUpExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignUpExample
+//
+//  Created by KSH on 12/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
